4116493d7478e9dbf7f2d26bd6513fd906332d3f
Kumaradoss <76904568+hytrel16@users.noreply.github.com>
2024-08-13 15:26:33 +0530

    Create itertools.product().py

---
+++ hackerRank/itertools.product().py
@@ -0,0 +1,11 @@
+from itertools import product
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+print(*list(product(A, B)))    
+
+
+#Output:
+# 1 2 3
+# 4 5
+# (1, 4) (1, 5) (2, 4) (2, 5) (3, 4) (3, 5)
