5fab928dfd119fb9b10a59cd4e5b6b34dc6bccef
Kumaradoss <76904568+hytrel16@users.noreply.github.com>
2024-08-13 15:28:23 +0530

    Update itertools.product().py

--- hackerRank/itertools.product().py
+++ hackerRank/itertools.product().py
@@ -8,4 +8,6 @@ print(*list(product(A, B)))
 # Output:
 # 1 2 3
 # 4 5
-# (1, 4) (1, 5) (2, 4) (2, 5) (3, 4) (3, 5)
+# product(A, B) generates: (1, 4), (1, 5), (2, 4), (2, 5), (3, 4), (3, 5)
+# list(product(A, B)) converts this into: [(1, 4), (1, 5), (2, 4), (2, 5), (3, 4), (3, 5)]
+# print(*list(product(A, B))): (1, 4) (1, 5) (2, 4) (2, 5) (3, 4) (3, 5)
